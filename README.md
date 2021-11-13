6ad4af378d0c89115eacb01ddae2d171f079ab8d
Àlex Serra <bounteous17@protonmail.com>
2021-11-13 19:39:31 +0100

    Add test for 'replaceAll' es2022 method

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # es2022-node-unit-test
+
+## Checking NodeJS ECMA version support
+
+Visit the website [node.green](https://node.green/) for checking the ECMA version supported by each different runtime version.
---
+++ __tests__/replaceAll.js
@@ -0,0 +1,23 @@
+describe('replaceAll', () => {
+    const originalMessage = 'Backbencher sits at the Back';
+
+    const crossExpect = message => {
+        const replacedMessage = 'Frontbencher sits at the Front';
+        expect(message).toBe(replacedMessage);
+    };
+
+    describe('previous ES2022 version', () => {
+        it('using a regex must replace all the charts matches', () => {
+            const message = originalMessage.replace(/Back/g, 'Front');
+            crossExpect(message);
+        });
+    });
+
+    describe('after ES2022 version', () => {
+        it('using a regex must replace all the charts matches', () => {
+            const message = originalMessage.replaceAll('Back', 'Front');
+            crossExpect(message);
+        });
+    });
+
+});
