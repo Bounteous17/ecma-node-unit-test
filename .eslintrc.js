49692434442716f8848335e8099d5d53ea83a508
Àlex Serra <bounteous17@protonmail.com>
2021-11-13 19:49:37 +0100

    Setup eslint package

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2022": true,
+        "jest": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13, // Same as 2022
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
